3dafc1c3b81f7207a9e6ac21987aae8a62231d28
Mike Aizatsky <mike.aizatsky@gmail.com>
2011-08-24 13:59:58 -0700

    fixed package problems

--- options.m
+++ options.m
@@ -1,21 +1,36 @@
 (* ::Package:: *)
 
 BeginPackage["FinancialOptions`"];
+
+MkOption::usage = 
+"MkOption[position, type, strike]
+	Creates new option object.";
+
+OptionPayoff::usage = 
+"OptionPayoff[option, assetprice]
+	Calculates option payoff.";
+
+PortfolioPayoff::usage = 
+"PortfolioPayoff[portfolio, assetprice]
+	Calculates portfolio payoff.";
+
 Begin["`Private`"];
 
-MkOption[aposition_, atype_, astrike_] := {position->aposition,type->atype,strike->astrike};
+MkOption[aposition_, atype_, astrike_] := 
+	{"position"->ToString[aposition],"type"->ToString[atype],"strike"->astrike};
 
 GetValue[option_, key_] := Replace[key, FilterRules[option,{key}][[1]]];
 
-OptionPosition[option_] := Switch[GetValue[option, position],
-	long, 1,
-	short, -1];
+OptionPosition[option_] := Switch[GetValue[option, "position"],
+	"long", 1,
+	"short", -1];
 
-OptionPayoff[option_, price_]:= OptionPosition[option] * Switch[GetValue[option, type],
-	call, Max[0, price - GetValue[option, strike]],
-	put, Max[0, GetValue[option, strike] - price]];
+OptionPayoff[option_, price_]:= 
+	OptionPosition[option] * Switch[GetValue[option, "type"],
+		"call", Max[0, price - GetValue[option, "strike"]],
+		"put", Max[0, GetValue[option, "strike"] - price]];
 
 PortfolioPayoff[portfolio_, price_]:=Fold[Plus, 0, OptionPayoff[#, price]& /@portfolio ];
 
-End[];
-EndPackage[];
+End[ ];
+EndPackage[ ];
